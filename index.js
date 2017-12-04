626c13940e796c9a604b4b8ff7e17200bcd60331
Boimb <atboimb@gmail.com>
2017-12-04 13:28:17 +0100

    add device creation + exec. Code Style fixes

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 // You could declare all your function there, but by convention and for good practices
 // you should only store functions in the './lib' folder and expose them here
 
+const install = require('./install')
+const myAwesomeFunction = require('./lib/myAwesomeFunction')
+const iLoveGladys = require('./lib/iLoveGladys')
+const getAGladysParam = require('./lib/getAGladysParam')
+const dbCalls = require('./lib/retrieveFromDb')
+const skySTheLimit = require('./lib/skySTheLimit')
+const newDevice = require('./lib/newDevice')
+const exec = require('./lib/exec')
+
 module.exports = function(sails) {
 
-        const install = require('./install')
-        const myAwesomeFunction = require('./lib/myAwesomeFunction')
-        const iLoveGladys = require('./lib/iLoveGladys')
-        const getAGladysParam = require('./lib/getAGladysParam')
-        const dbCalls = require('./lib/retrieveFromDb')
-        const skySTheLimit = require('./lib/skySTheLimit')
-
-    return {
-        install : install,
-        myAwesomeFunction : myAwesomeFunction,
-        iLoveGladys : iLoveGladys,
-        // shortand ES6 declaration
-        getAGladysParam,
-        // You can have an object of functions instead of just a function.
-        // e.g you'll have to call dbCalls.getAllUsers()
-        dbCalls,
-        skySTheLimit
-    }
+  return {
+    install : install,
+    myAwesomeFunction : myAwesomeFunction,
+    iLoveGladys : iLoveGladys,
+    // shortand ES6 declaration
+    getAGladysParam,
+    // You can have an object of functions instead of just a function.
+    // e.g you'll have to call dbCalls.getAllUsers()
+    dbCalls,
+    skySTheLimit,
+    exec
+  }
 };
--- install.js
+++ install.js
@@ -2,13 +2,13 @@ const Promise = require('bluebird');
 
 module.exports = function install() {
 
-    return new Promise(function(resolve, reject) {
-        // async work here
-        const valueToReturn = console.log('This will appear just one time...');
+  return new Promise(function (resolve, reject) {
+    // async work here
+    const valueToReturn = console.log('This will appear just one time...');
 
-        resolve(valueToReturn);
+    resolve(valueToReturn);
 
-        // if something fails, reject(new Error('bad bad'));
-    })
+    // if something fails, reject(new Error('bad bad'));
+  })
 
 };
