b9f04e88a17f7d31a1685f70e60a6d98dea78b05
Boimb <atboimb@gmail.com>
2018-10-29 17:05:11 +0100

    wip - testing new feature in the main Gladys code

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 
-module.exports = function install() {
-
+module.exports = function install(moduleInfos) {
+  sails.log.debug('moduleInfos: ', moduleInfos)
   return new Promise(function (resolve, reject) {
     // async work here
     const valueToReturn = console.log('This will appear just one time...');
